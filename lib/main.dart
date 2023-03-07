4ee0d9073a98d2494dcf4ed727bde2891a804d0a
habeebu <habeebrh321@gmail.com>
2023-03-07 12:59:00 +0530

    notification integration finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
+import 'package:app_notify/services/push_notification_service.dart';
 import 'package:app_notify/view/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+  RemoteMessage? initialMessage =
+      await FirebaseMessaging.instance.getInitialMessage();
+  if (initialMessage != null) {
+    // App received a notification when it was killed
+  }
+  await PushNotificationService().setupInteractedMessage();
+
+  await Permission.notification.isDenied.then(
+    (bool value) {
+      if (value) {
+        Permission.notification.request();
+      }
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
